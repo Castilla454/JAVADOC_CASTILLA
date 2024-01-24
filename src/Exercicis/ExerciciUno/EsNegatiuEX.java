ee4c25e62e4303e194cf1e3f026fd689932cd83c
Castilla454 <castilla4454@gmail.com>
2024-01-24 09:12:16 +0100

    hola

---
+++ src/Exercicis/ExerciciUno/EsNegatiuEX.java
@@ -0,0 +1,13 @@
+package Exercicis.ExerciciUno;
+
+    public class EsNegatiuEX extends Exception {
+        public EsNegatiuEX() {
+            Javadoc ___11
+            super("el valor no pot ser negatiu");
+        }
+
+        public EsNegatiuEX(String msg) {
+            super(msg);
+        }
+    }
+
